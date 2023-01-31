5039fa78eae3f29ddc1f21a410547c423781ce1d
alvaroeduardo <alvaroedusilva@yahoo.com>
2023-01-31 18:46:25 -0300

    Trabalhando com If

---
+++ AprendendoCSharp/7-Condicionais/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int media = 6;
+
+        if ( media >= 7)
+        {
+            Console.WriteLine("Aluno Aprovado.");
+
+        } else if ( media < 7 && media > 5 )
+        {
+            Console.WriteLine("Aluno de recuperação.");
+        } else
+        {
+            Console.WriteLine("Aluno reprovado.");
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
