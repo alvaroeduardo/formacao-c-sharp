c32f0029c12de0c78a4f5b816b7737e602f1c386
alvaroeduardo <alvaroedusilva@yahoo.com>
2023-01-31 11:22:18 -0300

    Trabalhando com caracteres

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
